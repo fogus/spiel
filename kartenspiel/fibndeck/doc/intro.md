14c9594ef1c3998a6a6a1130cf8af6a973c91f14
fogus <mefogus@gmail.com>
2014-07-18 09:22:21 -0400

    First cut at a fibdeck

---
+++ kartenspiel/fibndeck/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fibndeck
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ kartenspiel/fibndeck/test/fibndeck/core_test.clj
@@ -0,0 +1,7 @@
+(ns fibndeck.core-test
+  (:require [clojure.test :refer :all]
+            [fibndeck.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
