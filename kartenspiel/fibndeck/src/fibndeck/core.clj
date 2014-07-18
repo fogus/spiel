9779e5cbdb637a2c0510e96cd96a1fda14af1cdb
fogus <mefogus@gmail.com>
2014-07-18 11:21:42 -0400

    Suit reprs

--- kartenspiel/fibndeck/src/fibndeck/core.clj
+++ kartenspiel/fibndeck/src/fibndeck/core.clj
@@ -3,40 +3,40 @@
 
 (comment
 
-  (def fibn [{:rank 0,  :suit :circle,   :color :blue}
-             {:rank 13, :suit :circle,   :color :blue}
-             {:rank 5,  :suit :ku,       :color :yellow}
-             {:rank 8,  :suit :ku,       :color :yellow}
-             {:rank 13, :suit :ku,       :color :yellow}
-             {:rank 5,  :suit :heart,    :color :purple}
-             {:rank 8,  :suit :heart,    :color :purple}
-             {:rank 13, :suit :heart,    :color :purple}
-             {:rank 13, :suit :heart,    :color :purple}
-             {:rank 13, :suit :heart,    :color :purple}
-             {:rank 3,  :suit :triangle, :color :red, :pips 1}
-             {:rank 5,  :suit :triangle, :color :red, :pips 2}
-             {:rank 8,  :suit :triangle, :color :red, :pips 3}
-             {:rank 8,  :suit :triangle, :color :red, :pips 1}
-             {:rank 13, :suit :triangle, :color :red, :pips 2}
-             {:rank 13, :suit :triangle, :color :red, :pips 3}
-             {:rank 1,  :suit :square,   :color :green}
-             {:rank 2,  :suit :square,   :color :green}
-             {:rank 3,  :suit :square,   :color :green}
-             {:rank 5,  :suit :square,   :color :green}
-             {:rank 8,  :suit :square,   :color :green}
-             {:rank 8,  :suit :square,   :color :green}
-             {:rank 13, :suit :square,   :color :green}
-             {:rank 13, :suit :square,   :color :green}
-             {:rank 13, :suit :square,   :color :green}
-             {:rank 1,  :suit :shogi,    :color :orange}
-             {:rank 2,  :suit :shogi,    :color :orange}
-             {:rank 3,  :suit :shogi,    :color :orange}
-             {:rank 5,  :suit :shogi,    :color :orange}
-             {:rank 8,  :suit :shogi,    :color :orange}
-             {:rank 8,  :suit :shogi,    :color :orange}
-             {:rank 13, :suit :shogi,    :color :orange}
-             {:rank 13, :suit :shogi,    :color :orange}
-             {:rank 13, :suit :shogi,    :color :orange}
+  (def fibn [{:rank 0,  :suit :circle,   :color :blue          :unicode :○  }
+             {:rank 13, :suit :circle,   :color :blue          :unicode :○  }
+             {:rank 5,  :suit :ku,       :color :yellow        :unicode :く }
+             {:rank 8,  :suit :ku,       :color :yellow        :unicode :く }
+             {:rank 13, :suit :ku,       :color :yellow        :unicode :く }
+             {:rank 5,  :suit :heart,    :color :purple        :unicode :♡  }
+             {:rank 8,  :suit :heart,    :color :purple        :unicode :♡  }
+             {:rank 13, :suit :heart,    :color :purple        :unicode :♡  }
+             {:rank 13, :suit :heart,    :color :purple        :unicode :♡  }
+             {:rank 13, :suit :heart,    :color :purple        :unicode :♡  }
+             {:rank 3,  :suit :triangle, :color :red, :pips 1  :unicode :◬  }
+             {:rank 5,  :suit :triangle, :color :red, :pips 2  :unicode :▲  }
+             {:rank 8,  :suit :triangle, :color :red, :pips 3  :unicode :∴  }
+             {:rank 8,  :suit :triangle, :color :red, :pips 1  :unicode :◬  }
+             {:rank 13, :suit :triangle, :color :red, :pips 2  :unicode :▲  }
+             {:rank 13, :suit :triangle, :color :red, :pips 3  :unicode :∴  }
+             {:rank 1,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 2,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 3,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 5,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 8,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 8,  :suit :square,   :color :green         :unicode :□  }
+             {:rank 13, :suit :square,   :color :green         :unicode :□  }
+             {:rank 13, :suit :square,   :color :green         :unicode :□  }
+             {:rank 13, :suit :square,   :color :green         :unicode :□  }
+             {:rank 1,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 2,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 3,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 5,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 8,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 8,  :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 13, :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 13, :suit :shogi,    :color :orange        :unicode :☖  }
+             {:rank 13, :suit :shogi,    :color :orange        :unicode :☖  }
              ])
   
   (-> fibn
@@ -47,6 +47,9 @@
 
 
 
+  )
+
+(defn pprint-deck [deck]
   )
 
 
